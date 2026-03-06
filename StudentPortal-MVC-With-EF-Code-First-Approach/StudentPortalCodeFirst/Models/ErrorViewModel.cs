508dc632c5bb3660ff378d68668b8277b1af200a
Aryan Narayan <aryanshrivastava788@gmail.com>
2026-03-06 16:00:53 +0530

    StudentPortal Project(MVC with EF, Code First Approach)

---
+++ StudentPortal-MVC-With-EF-Code-First-Approach/StudentPortalCodeFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentPortalCodeFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ StudentPortal-MVC-With-EF-Code-First-Approach/StudentPortalCodeFirst/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentPortalCodeFirst.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
