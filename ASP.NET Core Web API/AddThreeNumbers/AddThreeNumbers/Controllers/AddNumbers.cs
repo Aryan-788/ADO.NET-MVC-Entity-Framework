18c46f64a1706c8b47f5d65963ebccf1e1dd33cb
Aryan Narayan <aryanshrivastava788@gmail.com>
2026-03-09 23:41:23 +0530

    Learning ASP.NET Core Web API and Added Some Project's Based on Web-API

---
+++ ASP.NET Core Web API/AddThreeNumbers/AddThreeNumbers/Controllers/AddNumbers.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AddThreeNumbers.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddNumbers : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index(int a, int b, int c)
+        {
+            var result = new
+            {
+                FirstNumber = a,
+                SecondNumber = b,
+                ThirdNumber = c,
+                Sum = a + b + c
+
+            };
+           
+            return Ok(result);
+        }
+    }
+}
