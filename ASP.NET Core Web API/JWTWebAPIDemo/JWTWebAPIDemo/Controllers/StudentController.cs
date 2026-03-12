2e2d5af431fa19c4fabf36ea77aa8cec65525237
Aryan Narayan <aryanshrivastava788@gmail.com>
2026-03-13 01:31:54 +0530

    Learning How to Protect our Web API using JWT Authentication and Added One JWT based Web-API Project

---
+++ ASP.NET Core Web API/JWTWebAPIDemo/JWTWebAPIDemo/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTWebAPIDemo.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetStudents()
+        {
+            return Ok(new string[]
+            {
+            "Arun",
+            "Divya",
+            "Rahul"
+            });
+        }
+    }
+}
--- ASP.NET Core Web API/SimpleWebApi/SimpleWebApi/obj/Debug/net10.0/SimpleWebApi.AssemblyInfo.cs
+++ ASP.NET Core Web API/SimpleWebApi/SimpleWebApi/obj/Debug/net10.0/SimpleWebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75d36e7339d7a0bdd733225a0c014f6f028a9fa1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
