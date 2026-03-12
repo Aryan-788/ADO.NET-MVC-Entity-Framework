1a4c83acfb5e1b524bbd022a783cbbbaab768eec
Aryan Narayan <aryanshrivastava788@gmail.com>
2026-03-13 01:45:03 +0530

    Learning Middleware and Added One WEB-API Project based on Middleware

---
+++ ASP.NET Core Web API/TrimMiddlewareAPI/TrimMiddlewareAPI/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TrimMiddlewareAPI.Models;
+
+namespace TrimMiddlewareAPI.Controllers
+{
+
+    [ApiController]
+    [Route("api/student")]
+    public class StudentController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post(Student student)
+        {
+            var name = student.Name;
+
+            // Reverse the string
+            char[] arr = name.ToCharArray();
+            Array.Reverse(arr);
+
+            var reversed = new string(arr);
+
+            return Ok(reversed);
+        }
+    }
+}
---
+++ ASP.NET Core Web API/TrimMiddlewareAPI/TrimMiddlewareAPI/Models/Student.cs
@@ -0,0 +1,7 @@
+﻿namespace TrimMiddlewareAPI.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+    }
+}
